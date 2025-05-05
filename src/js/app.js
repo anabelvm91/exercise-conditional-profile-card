a4dd487afc06a95b0e671cc095c3efa79a0cc55d
Anabel VM <anabelvivasm@gmail.com>
2025-05-05 12:10:15 +0000

    perfilcondicional

--- src/js/app.js
+++ src/js/app.js
@@ -23,27 +23,35 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log("These are the current variables: ", variables);
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  if (!variables.includeCover) cover = "<div class='cover'></div>";
+
+  document.querySelector("#widget_content").innerHTML = `
+        <div class="widget">
+          ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name || "Nombre"} ${variables.lastName ||
+    "Apellido"}</h1>
+          <h2>${variables.role || "Rol"}</h2>
+          <h3>${variables.city || "Ciudad"}, ${variables.country || "País"}</h3>
+          <ul class="position-${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${
+              variables.linkedin
+            }" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram
+            }" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
-    `;
+      `;
 }
 
 /**
@@ -51,24 +59,20 @@ function render(variables = {}) {
  */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should show the cover image
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
-    socialMediaPosition: "position-left",
-    // social media usernames
-    twitter: null,
-    github: null,
-    linkedin: null,
-    instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    background:
+      "https://images.unsplash.com/photo-1503264116251-35a269479413?ixlib=rb-4.0.3&auto=format&fit=crop&w=870&q=80",
+    avatarURL: "https://randomuser.me/api/portraits/women/44.jpg",
+    socialMediaPosition: "left",
+    twitter: "anabelvivas",
+    github: "anabelvivas",
+    linkedin: "anabelvivas",
+    instagram: "https://www.instagram.com/anavivasmata/?next=%2F",
+    name: "Anabel",
+    lastName: "Vivas",
+    role: "Full Stack Developer",
+    country: "España",
+    city: "Huelva"
   };
   render(window.variables); // render the card for the first time
 
